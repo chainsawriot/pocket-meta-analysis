e971f5cd2ee3d25583dffc3e0d03a09f90574585
chainsawriot <chainsawtiney@gmail.com>
2015-04-28 14:58:06 +0800

    update

--- pocket.R
+++ pocket.R
@@ -2,7 +2,7 @@ require(metafor)
 require(stringr)
 
 
-pocket <- read.csv("pocket_cable.csv", stringsAsFactors = FALSE)
+pocket <- read.csv("pocketdataapr28.csv", stringsAsFactors = FALSE)
 pocket <- pocket[pocket[,3]==1,]
 sum(as.numeric(pocket[,11]))
 
@@ -12,18 +12,22 @@ table(str_trim(pocket[,14]) == "UKN") # Response rate
 
 
 yi <- as.numeric(pocket[,15]) / 100
-vi <- (yi*(1-yi)) / as.numeric(pocket[,11])
+vi <- (yi*(1-yi)) / (as.numeric(pocket[,11]) - 1)
 forest(rma(yi=yi, vi=vi, slab=paste(str_trim(pocket[,8]), pocket[,5]), method="REML"))
 
-wi <- 1/sqrt(vi) #weighted by inversed variance
+metavi <- weights(rma(yi=yi, vi=vi, slab=paste(str_trim(pocket[,8]), pocket[,5]), method="REML"))
+
+wi <- 1/sqrt(vi) #weighted by inversed se, only for plotting
 size  <- 0.5 + 3.0 * (wi - min(wi))/(max(wi) - min(wi))
-loessdat <- data.frame(y = yi*100, x = as.Date(pocket[,5], format="%m/%d/%Y"))
-loessfit <- loess(y~as.numeric(x), weights = vi, data=loessdat, span=0.75)
+loessdat <- data.frame(y = yi, x = as.Date(pocket[,5], format="%m/%d/%Y"))
+loessfit <- loess(y~as.numeric(x), weights = metavi, data=loessdat, span=0.75)
 
 univ <- rep(1, 29)
 univ[9:24] <- 19
+univ[30] <- 19
 univcol <- rep('black', 29)
 univcol[9:24] <- 'blue'
+univcol[30] <- 'blue'
 
 
 png("timetrendc.png", width = 16, height = 10, units='in', res = 300)
@@ -31,10 +35,10 @@ plot(as.Date(pocket[,5], format="%m/%d/%Y"),(yi*100), cex = size, ylim=c(20,70),
 text(as.Date(pocket[,5], format="%m/%d/%Y"),(yi*100), pocket[,8], cex = 1.1, pos = 4, col = 'blue')
 plx <- predict(loessfit, newdata = as.numeric(seq(from = min(loessdat$x), to = max(loessdat$x), by = 1)), se = TRUE)
 abline(h=c(50), col = 'grey', lty = 2)
-lines(as.numeric(seq(from = min(loessdat$x), to = max(loessdat$x), by = 1)), plx$fit, lwd = 2, col = 'blue')
+lines(as.numeric(seq(from = min(loessdat$x), to = max(loessdat$x), by = 1)), (plx$fit*100), lwd = 2, col = 'blue')
 xseq <- as.numeric(seq(from = min(loessdat$x), to = max(loessdat$x), by = 1))
-highrange <- plx$fit + qt(0.975, plx$df)*plx$se
-lowrange <- plx$fit - qt(0.975, plx$df)*plx$se
+highrange <- (plx$fit + qt(0.975, plx$df)*plx$se) * 100
+lowrange <- (plx$fit - qt(0.975, plx$df)*plx$se) * 100
 polygon(c(xseq, rev(xseq)), c(highrange, rev(lowrange)), border = NA, col = rgb(0,0,1,0.1))
 dev.off()
 
